1875261c0733327815b1d01ebce5cf6e5fe006a8
akter97 <akterhossenraj2040@gmail.com>
2026-02-27 01:29:47 +0600

    up

--- config/db.js
+++ config/db.js
@@ -1,24 +1,28 @@
-const mysql = require('mysql2'); // অথবা 'mysql'
+const mysql = require('mysql2');
+const fs = require('fs');
 require('dotenv').config();
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "friendsclub",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   multipleStatements: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  ssl: {
+    ca: fs.readFileSync(__dirname + '/ca.pem') // point to Aiven CA
+  }
 });
 
- 
 db.getConnection((err, connection) => {
   if (err) {
     console.error("Database connection failed: " + err.message);
   } else {
-    console.log("MySQL Connected via Pool");
-    connection.release(); 
+    console.log("MySQL Connected via Pool!");
+    connection.release();
   }
 });
 
